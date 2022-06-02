5a3eedb95a12dedd50690a87ddca3c5c62f0196b
satish <13929851+sateeshmachineni@users.noreply.github.com>
2022-06-02 12:42:26 +0530

    Update index.php

--- webApp/index.php
+++ webApp/index.php
@@ -26,15 +26,17 @@
 
        <?php
 
-         include '/var/www/config.php';
+        
 
-         $conn = new mysqli($server, $username, $password, $dbname);
+         $conn = new mysqli("dbins1.cebrrxbs8omn.us-east-1.rds.amazonaws.com", "admin", "labuser1234", "db1");
 
          if ($conn->connect_error)
          {
          die("Connection failed: " . $conn->connect_error);
          }
-
+         
+        echo -e "$conn";
+      
          $sql = "SELECT * FROM images";
          $result = $conn->query($sql);
 
